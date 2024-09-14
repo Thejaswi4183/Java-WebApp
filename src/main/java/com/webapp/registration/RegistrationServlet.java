f26a1e6ec71382c276fdccded0ed0cf7cb8be3d8
Thejaswi S <thejaswi4uns@gmail.com>
2024-09-14 22:46:06 +0530

    Modified

---
+++ src/main/java/com/webapp/registration/RegistrationServlet.java
@@ -0,0 +1,30 @@
+package com.webapp.registration;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/register")
+public class RegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String uname=request.getParameter("name");
+		String uemail=request.getParameter("email");
+		String upwd=request.getParameter("pass");
+		String umobile=request.getParameter("contact");
+		PrintWriter out = response.getWriter();
+		try {
+			
+		}
+		catch{
+			
+		}
+	}
+
+}
